c27e23bb96718d829911bb7aa57d7c7bdc4526ef
Izzat <tdmkenzie@gmail.com>
2026-04-10 14:34:23 +0500

    fixed: render3

--- src/auth/google.strategy.ts
+++ src/auth/google.strategy.ts
@@ -14,7 +14,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       clientID: configService.getOrThrow('GOOGLE_CLIENT_ID'),
       clientSecret: configService.getOrThrow('GOOGLE_CLIENT_SECRET'),
       callbackURL: configService.getOrThrow('GOOGLE_CALLBACK_URL'),
-      scope: ['email', 'profile'],
+      scope: ['email', 'profile', `https://www.googleapis.com/auth/drive`],
     });
   }
 
